035df6769674ce27ed7064e63feb240f2b08aa2b
Ilya <ilya@genly.com>
2020-06-14 19:22:53 -0700

    modified setup.py file

--- setup.py
+++ setup.py
@@ -27,4 +27,9 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=[
+            "numpy",
+            "scipy",
+            "scikit-learn",
+        ]
 )
